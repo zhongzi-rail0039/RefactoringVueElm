00dc3cca33796819b423eb4f38f14750d1a8bab1
粽子 <rail_0039@163.com>
2019-03-07 15:27:34 +0800

    配置数据接口请求

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const mockData = require('./src/mock/data.json')
+const seller = mockData.seller
+const goods = mockData.goods
+const ratings = mockData.ratings
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +17,29 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    port: 9090,
+    before (app, server) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   }
 }

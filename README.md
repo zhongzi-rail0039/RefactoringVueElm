4e2ec4b53bf6d2f7ecd96d8325c5f4496807b039
粽子 <rail_0039@163.com>
2019-03-07 21:40:48 +0800

    公共初始化样式定义

--- README.md
+++ README.md
@@ -3,12 +3,15 @@
 > [图标字体制作](https://icomoon.io/)
 
 - 1：导入自己的SVG图片
+
 ![](./describe-img/1.png)
 
 - 2：选中所有的字体图片，然后下载
+
 ![](./describe-img/2.png)
 
 - 3：修改文件名
+
 ![](./describe-img/3.png)
 
 - 4：下载即可
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <cube-button>Button</cube-button>
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <cube-button :primary="true">Primary Button</cube-button>
+    <cube-popup type="my-popup" ref="myPopup">
+      My Popup Content 1
+    </cube-popup>
+    <cube-button @click="showPopup('myPopup')">
+      Show Popup
+    </cube-button>
+    <p class="tip-eg">
+      <span>cube-ui</span>
+      <cube-tip
+        ref="tip2"
+        :direction="direction"
+        :style="tipStyle"
+        @close="close"
+        @click="clickHandler">
+        <div>Awesome!</div>
+      </cube-tip>
+    </p>
+    <cube-button @click="showTip('bottom')">top</cube-button>
+    <cube-button @click="showTip('top')">bottom</cube-button>
+    <cube-button @click="showTip('right')">left</cube-button>
+    <cube-button @click="showTip('left')">right</cube-button>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app'
-  // components: {
-  //   HelloWorld
-  // }
+  name: 'app',
+  data () {
+    return {
+      direction: '',
+      tipStyle: ''
+    }
+  },
+  methods: {
+    showPopup (refId) {
+      const component = this.$refs[refId]
+      component.show()
+      setTimeout(() => {
+        component.hide()
+      }, 1000)
+    },
+    showTip (direction) {
+      this.direction = direction
+      this.$refs.tip2.show()
+
+      switch (direction) {
+        case 'top':
+          this.tipStyle = 'left: 100px; top: 30px;'
+          break
+        case 'bottom':
+          this.tipStyle = 'left: 100px; top: -50px;'
+          break
+        case 'left':
+          this.tipStyle = 'left: 200px; top: -10px;'
+          break
+        case 'right':
+          this.tipStyle = 'left: 2px; top: -10px;'
+          break
+      }
+    },
+    close () {
+      console.log('click close button')
+    },
+    clickHandler () {
+      console.log('click tip area')
+    }
+  },
+  components: {
+    HelloWorld
+  }
 }
 </script>
 <style lang="stylus">
-#app
-  /*font-family 'Avenir', Helvetica, Arial, sans-serif*/
-  /*-webkit-font-smoothing antialiased*/
-  /*-moz-osx-font-smoothing grayscale*/
-  /*text-align center*/
-  /*color #2c3e50*/
-  /*margin-top 60px*/
 </style>

8a7bb1e5bd2b733e984b6048c3b787fdfd093d44
粽子 <rail_0039@163.com>
2019-04-11 22:20:48 +0800

    头部组件页面

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'seller'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-tab/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tab">
+    1123213
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
+
+<style scoped>
+
+</style>
